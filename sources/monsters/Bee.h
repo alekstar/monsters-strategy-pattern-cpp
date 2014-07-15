eb37d004d124479863583082bd9567d79e2650b2
Aleksandr Tarasenko <work.tarasenko@gmail.com>
2014-07-15 11:34:42 +0300

    added class Bee

---
+++ sources/monsters/Bee.h
@@ -0,0 +1,26 @@
+#ifndef BEE_H
+#define	BEE_H
+#include "Monster.h"
+#include "Poison.h"
+#include "Fly.h"
+
+namespace Monsters
+{
+    class Bee : public Monster
+    {
+    public:
+        Bee()
+        {
+            setMoveBehaviour(std::shared_ptr<MoveBehavior>(new Fly()));
+            setAbility(std::shared_ptr<Ability>(new Poison()));
+        }
+        
+        void display()
+        {
+            std::cout << "I'm a bee!" << std::endl;
+        }
+    };
+}
+
+#endif	/* BEE_H */
+
